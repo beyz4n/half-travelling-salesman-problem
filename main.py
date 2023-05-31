186287d54d1001f61613844130400c086597b582
beyz4n <95589101+beyz4n@users.noreply.github.com>
2023-06-01 01:18:07 +0300

    density function added and some improvements were made.

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
+import random
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from sklearn.cluster import OPTICS
 import numpy as np
 import time
+import math
+import gc
+from scipy.stats import pearsonr
+import scipy.stats as stats
+from itertools import cycle
+from sklearn.cluster import OPTICS
+
+def calculate_density(elm,cent_x, cent_y):
+    density = 0
+    for i in range(0, elm.size, 2):
+        density += math.sqrt((cent_x - elm[i])**2 + (cent_y - elm[i+1])**2)
+    return density/(elm.size/2)
 
 # File reading and assigning the values
+colors = ['aqua', 'aquamarine', 'azure', 'beige', 'black', 'blue', 'brown', 'chartreuse', 'chocolate', 'coral', 'crimson', 'cyan', 'darkblue', 'darkgreen', 'fuchsia', 'gold', 'goldenrod', 'green', 'grey', 'indigo', 'ivory', 'khaki', 'lavender', 'lightblue', 'lightgreen', 'lime', 'magenta', 'maroon', 'navy', 'olive', 'orange', 'orangered', 'orchid', 'pink', 'plum', 'purple', 'red','salmon', 'sienna', 'silver', 'tan', 'teal', 'tomato', 'turquoise', 'violet', 'wheat', 'yellow' , 'yellowgreen'  ]
+random.shuffle(colors)
 start = time.time()
 with open(input("Enter a file name: "), 'r') as file:
     line = file.readline()
     listPoints = []
-    x_points = []
-    y_points = []
     while file and line:
         line = line.strip()
         line_sep = line.split(" ")
         digits = [int(x) for x in line_sep if x.isdigit()]
         listPoints.append((digits[1], digits[2]))
-        x_points.append(digits[1])
-        y_points.append(digits[2])
         line = file.readline()
 
-X = np.array(listPoints)
-kmeans = KMeans(n_clusters=8, init='k-means++', random_state=0, n_init='auto')
+X = np.array(listPoints, dtype='int')
+del listPoints
+gc.collect()
+cluster_size = math.ceil(X.size/1500)
+kmeans = KMeans(n_clusters=cluster_size, init='k-means++', random_state=0, n_init='auto')
 y = kmeans.fit_predict(X)
 cent = kmeans.cluster_centers_
-plt.scatter(X[y==0, 0], X[y==0, 1], s=1, c='red', label ='Cluster 1')
-plt.scatter(X[y==1, 0], X[y==1, 1], s=1, c='blue', label ='Cluster 2')
-plt.scatter(X[y==2, 0], X[y==2, 1], s=1, c='green', label ='Cluster 3')
-plt.scatter(X[y==3, 0], X[y==3, 1], s=1, c='purple', label ='Cluster 4')
-plt.scatter(X[y==4, 0], X[y==4, 1], s=1, c='pink', label ='Cluster 5')
-plt.scatter(X[y==5, 0], X[y==5, 1], s=1, c='black', label ='Cluster 6')
-plt.scatter(X[y==6, 0], X[y==6, 1], s=1, c='cyan', label ='Cluster 7')
-plt.scatter(X[y==7, 0], X[y==7, 1], s=1, c='magenta', label ='Cluster 8')
+
+# std - z score
+st_dev = []
+pearson_corr = []
+norms = []
+elements = np.array(X.flatten())
+for i in range(cluster_size):
+    r = np.round(np.random.rand(), 1)
+    g = np.round(np.random.rand(), 1)
+    b = np.round(np.random.rand(), 1)
+    st_dev.append(calculate_density(elements, cent[i][0], cent[i][1]))
+    plt.scatter(X[y == i, 0], X[y == i, 1], s=1, color=[r,g,b])
+    print(st_dev[i])
+    print(cent[i])
+    # pearson_corr.append(pearsonr(X[y==i, 0],X[y==i, 1]))
+    # norms.append(stats.norm.pdf(X))
+    # scipy.stats.multivariate_normal()
+
+plt.scatter(cent[:, 0], cent[:, 1], s=20, c = 'yellow')
 
 # optics = OPTICS(min_samples=1000, xi=0.05, min_cluster_size=0.05).fit(X)
 # labels = optics.labels_
