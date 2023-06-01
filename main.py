5eb0fd3fe1c1084e61858e3172c85606f5655bcc
beyz4n <95589101+beyz4n@users.noreply.github.com>
2023-06-01 21:08:54 +0300

    some adjustments for feature selection

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import random
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import numpy as np
 import time
 import math
 import gc
-from scipy.stats import pearsonr
-import scipy.stats as stats
-from itertools import cycle
-from sklearn.cluster import OPTICS
 
-def calculate_density(elm,cent_x, cent_y):
+
+def calculate_density(elm, cent_x, cent_y):
     density = 0
-    for i in range(0, elm.size, 2):
-        density += math.sqrt((cent_x - elm[i])**2 + (cent_y - elm[i+1])**2)
+    for j in range(0, elm.size, 2):
+        density += math.sqrt((cent_x - elm[j])**2 + (cent_y - elm[j+1])**2)
     return density/(elm.size/2)
+
 
 # File reading and assigning the values
 start = time.time()
@@ -28,10 +25,11 @@ with open(input("Enter a file name: "), 'r') as file:
         listPoints.append((digits[1], digits[2]))
         line = file.readline()
 
+size = int(digits[0]) + 1
 X = np.array(listPoints, dtype='int')
 del listPoints
 gc.collect()
-cluster_size = math.ceil(X.size/1500)
+cluster_size = math.ceil(size/750)
 kmeans = KMeans(n_clusters=cluster_size, init='k-means++', random_state=0, n_init='auto')
 y = kmeans.fit_predict(X)
 cent = kmeans.cluster_centers_
@@ -46,12 +44,15 @@ if cluster_size > 1:
         b = np.round(np.random.rand(), 1)
         elements = np.array(X[y == i].flatten())
         densities.append(calculate_density(elements, cent[i][0], cent[i][1]))
-        plt.scatter(X[y == i, 0], X[y == i, 1], s=1, color=[r,g,b])
-        print(densities[i])
-        print(cent[i])
+        plt.scatter(X[y == i, 0], X[y == i, 1], s=1, color=[r, g, b])
         # pearson_corr.append(pearsonr(X[y==i, 0],X[y==i, 1]))
         # norms.append(stats.norm.pdf(X))
         # scipy.stats.multivariate_normal()
+    density_arr = np.array(densities)
+    ids = np.arange(cluster_size)
+    features = np.column_stack((ids, density_arr))
+    clusters = np.hstack((cent, features))
+    print(clusters)
 else:
     plt.scatter(cent[:, 0], cent[:, 1], s=20, c='yellow')
     plt.scatter(X[y == 0, 0], X[y == 0, 1], s=1, c='crimson')
